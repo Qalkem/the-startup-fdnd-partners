a39d3671fe3b2994cb3f9eddf0c772934faf905a
Jean C.M Rojer <76013244+Qalkem@users.noreply.github.com>
2022-01-27 14:10:08 +0100

    update mobile version
    update mobile version

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,18 @@
-document.open(, replace)
+
+
+ const FilterButton= document.getElementById('FilterButton');
+ const CloseButton= document.getElementById('CloseButton');
+ const Filter= document.getElementById('Filter')
+ FilterButton.addEventListener('click',() => {
+    
+     Filter.classList.add('Open') ;
+ });
+ CloseButton.addEventListener('click',() => {
+    
+     Filter.classList.remove('Open') ;
+ });
+ 
+ document.open(, replace)
 
 function move() {
     var elem = document.getElementById("myBar");   
